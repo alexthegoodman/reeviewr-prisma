2a60998d6d2bf3e5d43cda3225d22e7eb62389eb
Alex Woodman <alex@Alexs-MacBook-Pro-2.local>
2019-04-07 19:30:47 -0400

    working on truncate

--- modules/db/index.ts
+++ modules/db/index.ts
@@ -20,20 +20,8 @@ export async function destroyConnection() {
 
 export function getConnection() {
   if (!$connection) {
-    /*
-        Node types for Postgres types
-
-        - When the postgres driver encounters a *datetime*, it creates a JavaScript Date. Cool!
-        - When the postgres driver encounters a *time*, it creates a JavaScript string. Cool.
-        - When the postgres driver encounters a *date* (not implying a time of day), it creates a JavaScript Date. Boo!
-
-        This customizes the behavior to pass it through as a string, reducing the risk of time zone drift, etc.
-
-        https://stackoverflow.com/a/50717046/202907
-
-     */
-    var pgTypes = require("pg").types;
-    pgTypes.setTypeParser(1082, (val: string) => val);
+    // var mysqlTypes = require("mysql").types;
+    // mysqlTypes.setTypeParser(1082, (val: string) => val);
 
     $connection = knex(knexConfig);
   }
@@ -45,14 +33,15 @@ export function _setConnection(knex: Knex) {
 }
 
 export async function truncateAll(knex: Knex) {
-  const result = await knex.raw(`
-      SELECT table_name
-      FROM information_schema.tables
-      WHERE table_schema='public'
-      AND table_type='BASE TABLE';
-   `);
-  const tables: string[] = result.rows.map((r: any) => r.table_name);
-  const recordTables = tables.filter(t => !t.includes("knex"));
-  const escapedTableNameList = recordTables.map(n => `"${n}"`).join(", ");
-  await knex.raw(`TRUNCATE ${escapedTableNameList} CASCADE`);
+  // const result = await knex.raw(`
+  //     SELECT table_name
+  //     FROM information_schema.tables
+  //  `);
+  // console.info(result)
+  // const tables: string[] = result[0].map((r: any) => r.TABLE_NAME);
+  // console.info(tables)
+  // const recordTables = tables.filter(t => !t.includes("knex"));
+  // const escapedTableNameList = tables.map(n => `TRUNCATE TABLE ${n}; `).join("");
+  // await knex.raw(`${escapedTableNameList}`);
+  return;
 }
