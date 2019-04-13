272b075c358eae6ba2eaccf169633c78daebf90f
Alex Woodman <alex@Alexs-MacBook-Pro-2.local>
2019-04-13 14:42:45 -0400

    replace gql-gen with prisma generate

--- entry/scripts/main-queue-processor.ts
+++ entry/scripts/main-queue-processor.ts
@@ -1,14 +1,14 @@
-// This file is JS becaule we can't export the worker function correctly
-// when using ES modules
+// TODO: re-add /atomic-object/jobs/
 
-const { ALL_JOBS } = require("services");
-const { Context } = require("graphql-api/context");
-const {
-  makeJobProcessorFunction,
-} = require("atomic-object/jobs/processing-function");
+// const { ALL_JOBS } = require("services");
+// const { Context } = require("graphql-api/context");
 
-console.log("Loaded main queue worker");
-module.exports = makeJobProcessorFunction({
-  buildContext: () => new Context(),
-  jobs: ALL_JOBS.filter((job: any) => job.queue === "main"),
-});
+// const {
+//   makeJobProcessorFunction,
+// } = require("atomic-object/jobs/processing-function");
+
+// console.log("Loaded main queue worker");
+// module.exports = makeJobProcessorFunction({
+//   buildContext: () => new Context(),
+//   jobs: ALL_JOBS.filter((job: any) => job.queue === "main"),
+// });
