c174bd259ec4738a2b6deb73f898f8aff47a7c52
Alex Woodman <alex@Alexs-MacBook-Pro-2.local>
2019-04-16 18:18:23 -0400

    Track draft

--- modules/client/components/ui/Track/Track.tsx
+++ modules/client/components/ui/Track/Track.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 
 import { TrackProps } from "./Track.d";
+import TrackWaveForm from "../TrackWaveForm/TrackWaveForm";
+import TrackHeader from "../TrackHeader/TrackHeader";
+import TrackArt from "../TrackArt/TrackArt";
 
 const Track: React.FC<TrackProps> = ({
   ref = null,
@@ -8,7 +11,24 @@ const Track: React.FC<TrackProps> = ({
   onClick = e => console.info("Click"),
 }) => {
   const clickHandler = e => onClick(e);
-  return <></>;
+
+  return (
+    <section ref={ref} className={`track ${className}`}>
+      <div className="artContainer">
+        <TrackArt
+          imageUrl="https://via.placeholder.com/400"
+          altText="Reeviewr Track"
+        />
+      </div>
+      <div className="infoContainer">
+        <TrackHeader
+          trackTitle="All of the Lights"
+          artistName="Regina Spektor"
+        />
+        <TrackWaveForm />
+      </div>
+    </section>
+  );
 };
 
 export default Track;
