19e314b5a214d515e5c104862be6d14732581f63
Alex Woodman <alex@Alexs-MacBook-Pro-2.local>
2019-04-22 12:39:29 -0400

    additional logging

--- modules/client/services/ApolloClient.ts
+++ modules/client/services/ApolloClient.ts
@@ -16,10 +16,12 @@ const client = new ApolloClient({
       if (graphQLErrors)
         graphQLErrors.map(({ message, locations, path }) =>
           console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+            prismaUri
           )
         );
-      if (networkError) console.log(`[Network error]: ${networkError}`);
+      if (networkError)
+        console.log(`[Network error]: ${networkError}`, prismaUri);
     }),
     new HttpLink({
       uri: prismaUri,
