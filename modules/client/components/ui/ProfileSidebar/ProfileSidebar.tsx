7f6f180a0406e78777b08c63e07f952213695d9c
Alex Woodman <alex@Alexs-MacBook-Pro-2.local>
2019-06-09 18:11:17 -0400

    basic profile sidebar

--- modules/client/components/ui/ProfileSidebar/ProfileSidebar.tsx
+++ modules/client/components/ui/ProfileSidebar/ProfileSidebar.tsx
@@ -2,8 +2,10 @@ import * as React from "react";
 
 import { ProfileSidebarProps } from "./ProfileSidebar.d";
 import Sidebar from "../Sidebar/Sidebar";
-import { Text } from "@blueprintjs/core";
+import { Text, Icon } from "@blueprintjs/core";
 import Legacy from "../../../../services/Legacy";
+import Strings from "../../../services/Strings";
+import { SocialIcon } from "react-social-icons";
 
 const ProfileSidebar: React.FC<ProfileSidebarProps> = ({
   ref = null,
@@ -12,35 +14,60 @@ const ProfileSidebar: React.FC<ProfileSidebarProps> = ({
   userData = null,
 }) => {
   const legacy = new Legacy();
+  const strings = new Strings();
 
   const clickHandler = e => onClick(e);
 
-  const firstName = legacy.extractMetaValue(
-    userData.user.userMeta,
-    "firstName"
-  );
-  const lastName = legacy.extractMetaValue(userData.user.userMeta, "lastName");
-  const userArtistName = legacy.extractMetaValue(
-    userData.user.userMeta,
-    "userArtistName"
-  );
+  const userMetaList = legacy.extractMultipleMeta(userData.user.userMeta, [
+    "firstName",
+    "lastName",
+    "userArtistName",
+    "profileImage",
+    "facebook",
+    "twitter",
+    "bandcamp",
+    "soundcloud",
+    "userBio",
+  ]);
 
   return (
     <Sidebar type="sub">
-      {/* ArtistImage */}
-      <Text tagName="h1" className="sidebarHeadline">
-        {userArtistName}
-      </Text>
-      {/* Edit Profile */}
-      <Text
-        tagName="h2"
-        className="sidebarMeta"
-      >{`${firstName} ${lastName}`}</Text>
-      <Text tagName="h3" className="sidebarMeta">
-        {userData.user.userEmail}
-      </Text>
-      {/* Bio */}
-      {/* ArtistSocialMedia */}
+      <div className="subSidebarContain">
+        <img className="profileImage" src={userMetaList["profileImage"]} />
+        {userData.user.userEmail !== null ? (
+          <SocialIcon
+            url={`mailto:${userData.user.userEmail}`}
+            network="email"
+          />
+        ) : (
+          <></>
+        )}
+        {userMetaList["facebook"] !== "" ? (
+          <SocialIcon url={userMetaList["facebook"]} network="facebook" />
+        ) : (
+          <></>
+        )}
+        {userMetaList["twitter"] !== "" ? (
+          <SocialIcon url={userMetaList["twitter"]} network="twitter" />
+        ) : (
+          <></>
+        )}
+        {userMetaList["soundcloud"] !== "" ? (
+          <SocialIcon url={userMetaList["soundcloud"]} network="soundcloud" />
+        ) : (
+          <></>
+        )}
+
+        <Text tagName="h1" className="sidebarHeadline">
+          {strings.decode(userMetaList["userArtistName"])}
+        </Text>
+        {/* TODO: Edit Profile */}
+        <Text tagName="h2" className="sidebarMeta">{`${
+          userMetaList["firstName"]
+        } ${userMetaList["lastName"]}`}</Text>
+
+        <Text tagName="p">{strings.decode(userMetaList["userBio"])}</Text>
+      </div>
     </Sidebar>
   );
 };
