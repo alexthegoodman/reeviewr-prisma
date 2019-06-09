397f5c060faae4dcfb9fa9c6dc93873b6db34eb2
Alex Woodman <alex@Alexs-MacBook-Pro-2.local>
2019-06-09 18:33:06 -0400

    basic artist detail mobile

--- modules/client/components/ui/ProfileSidebar/ProfileSidebar.tsx
+++ modules/client/components/ui/ProfileSidebar/ProfileSidebar.tsx
@@ -33,40 +33,59 @@ const ProfileSidebar: React.FC<ProfileSidebarProps> = ({
   return (
     <Sidebar type="sub">
       <div className="subSidebarContain">
-        <img className="profileImage" src={userMetaList["profileImage"]} />
-        {userData.user.userEmail !== null ? (
-          <SocialIcon
-            url={`mailto:${userData.user.userEmail}`}
-            network="email"
-          />
-        ) : (
-          <></>
-        )}
-        {userMetaList["facebook"] !== "" ? (
-          <SocialIcon url={userMetaList["facebook"]} network="facebook" />
-        ) : (
-          <></>
-        )}
-        {userMetaList["twitter"] !== "" ? (
-          <SocialIcon url={userMetaList["twitter"]} network="twitter" />
-        ) : (
-          <></>
-        )}
-        {userMetaList["soundcloud"] !== "" ? (
-          <SocialIcon url={userMetaList["soundcloud"]} network="soundcloud" />
-        ) : (
-          <></>
-        )}
+        <div className="sidebarHeader">
+          <img className="profileImage" src={userMetaList["profileImage"]} />
+          <div className="mobileShow">
+            <Text tagName="h1" className="sidebarHeadline">
+              {strings.decode(userMetaList["userArtistName"])}
+            </Text>
+            {/* TODO: Edit Profile */}
+            <Text tagName="h2" className="sidebarMeta">{`${
+              userMetaList["firstName"]
+            } ${userMetaList["lastName"]}`}</Text>
+          </div>
+        </div>
+        <div className="sidebarBody">
+          <div className="socialLinks">
+            {userData.user.userEmail !== null ? (
+              <SocialIcon
+                url={`mailto:${userData.user.userEmail}`}
+                network="email"
+              />
+            ) : (
+              <></>
+            )}
+            {userMetaList["facebook"] !== "" ? (
+              <SocialIcon url={userMetaList["facebook"]} network="facebook" />
+            ) : (
+              <></>
+            )}
+            {userMetaList["twitter"] !== "" ? (
+              <SocialIcon url={userMetaList["twitter"]} network="twitter" />
+            ) : (
+              <></>
+            )}
+            {userMetaList["soundcloud"] !== "" ? (
+              <SocialIcon
+                url={userMetaList["soundcloud"]}
+                network="soundcloud"
+              />
+            ) : (
+              <></>
+            )}
+          </div>
 
-        <Text tagName="h1" className="sidebarHeadline">
-          {strings.decode(userMetaList["userArtistName"])}
-        </Text>
-        {/* TODO: Edit Profile */}
-        <Text tagName="h2" className="sidebarMeta">{`${
-          userMetaList["firstName"]
-        } ${userMetaList["lastName"]}`}</Text>
-
-        <Text tagName="p">{strings.decode(userMetaList["userBio"])}</Text>
+          <div className="mobileHide">
+            <Text tagName="h1" className="sidebarHeadline">
+              {strings.decode(userMetaList["userArtistName"])}
+            </Text>
+            {/* TODO: Edit Profile */}
+            <Text tagName="h2" className="sidebarMeta">{`${
+              userMetaList["firstName"]
+            } ${userMetaList["lastName"]}`}</Text>
+          </div>
+          <Text tagName="p">{strings.decode(userMetaList["userBio"])}</Text>
+        </div>
       </div>
     </Sidebar>
   );
