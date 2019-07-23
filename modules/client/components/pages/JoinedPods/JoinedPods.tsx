2ebab37eacc34208a83f9086f0a247a4a4798ae7
Alex Woodman <alex@Alexs-MacBook-Pro-2.local>
2019-07-23 07:25:43 -0400

    joined pods static

--- modules/client/components/pages/JoinedPods/JoinedPods.tsx
+++ modules/client/components/pages/JoinedPods/JoinedPods.tsx
@@ -1,9 +1,23 @@
 import * as React from "react";
 
+import CategoryGrid from "../../ui/CategoryGrid/CategoryGrid";
+import PodCard from "../../ui/PodCard/PodCard";
 import { JoinedPodsProps } from "./JoinedPods.d";
 
 const JoinedPods: React.FC<JoinedPodsProps> = () => {
-  return <></>;
+  return (
+    <CategoryGrid title="Joined Pods">
+      <PodCard />
+      <PodCard />
+      <PodCard />
+      <PodCard />
+      <PodCard />
+      <PodCard />
+      <PodCard />
+      <PodCard />
+      <PodCard />
+    </CategoryGrid>
+  );
 };
 
 export default JoinedPods;
