145141789f06c513a1686e16fc9ddda19092e1d2
Alex Woodman <alexthegoodman@gmail.com>
2020-05-07 13:52:35 -0400

    cleanup

--- modules/core/index.ts
+++
@@ -1,6 +0,0 @@
-import { Flavor } from "helpers";
-
-export type Percentage = Flavor<number, "percentage">;
-
-export type HourlyRate = Flavor<number, "money per hour">;
-export type DailyRate = Flavor<number, "money per day">;
--- modules/helpers/dataloader.ts
+++
@@ -1,8 +0,0 @@
-import * as DataLoader from "dataloader";
-export async function uncachedLoad<K, V>(
-  dataloader: DataLoader<K, V>,
-  key: K
-): Promise<V> {
-  dataloader.clear(key);
-  return dataloader.load(key);
-}
